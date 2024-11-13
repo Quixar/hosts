f0db9a4ee97f2e516fee53b7453003c06d5464dd
Quixar <evgenmart01@gmail.com>
2024-11-13 11:42:42 +0000

    init commit

--- verif.sh
+++ verif.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cat /etc/hosts | while read -r line; do
-   
+
     if [[ $line =~ ^# ]] || [[ -z $line ]]; then
         continue
     fi
 
-   
     ip_address=$(echo $line | awk '{print $1}')
     hostname=$(echo $line | awk '{print $2}')
 
-   
     nslookup_ip=$(nslookup $hostname 2>/dev/null | grep -A1 "Name:" | grep "Address:" | awk '{print $2}')
 
     if [[ "$nslookup_ip" && "$nslookup_ip" != "$ip_address" ]]; then
