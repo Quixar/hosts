f3f0b3a251615a9b0606318bfb41beefee6050b1
Quixar <evgenmart01@gmail.com>
2024-11-13 11:44:49 +0000

    second commit

--- verif.sh
+++ verif.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+validate_ip() {
+    local hostname=$1
+    local ip_address=$2
+    local dns_server=$3
+
+    nslookup_ip=$(nslookup "$hostname" "$dns_server" 2>/dev/null | grep -A1 "Name:" | grep "Address:" | awk '{print $2}')
+
+    if [[ "$nslookup_ip" && "$nslookup_ip" != "$ip_address" ]]; then
+        echo "Bogus IP for $hostname in /etc/hosts!"
+    fi
+}
+
 cat /etc/hosts | while read -r line; do
 
     if [[ $line =~ ^# ]] || [[ -z $line ]]; then
@@ -8,9 +21,6 @@ cat /etc/hosts | while read -r line; do
     ip_address=$(echo $line | awk '{print $1}')
     hostname=$(echo $line | awk '{print $2}')
 
-    nslookup_ip=$(nslookup $hostname 2>/dev/null | grep -A1 "Name:" | grep "Address:" | awk '{print $2}')
-
-    if [[ "$nslookup_ip" && "$nslookup_ip" != "$ip_address" ]]; then
-        echo "Bogus IP for $hostname in /etc/hosts!"
-    fi
+    validate_ip "$hostname" "$ip_address" "8.8.8.8"
 done
+
